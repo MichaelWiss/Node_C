f88e48bf3369c26b02682171cb1548138612580b
MichaelWiss <michael.wiss@gmail.com>
2017-02-05 14:55:21 -0600

    email regexp

--- assets/app/auth/signup.component.ts
+++ assets/app/auth/signup.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
 	selector: 'app-signup',
@@ -6,6 +7,18 @@ import { Component } from "@angular/core";
 })
 
 
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+	myForm: FormGroup;
 
+	ngOnInit() {
+		this.myForm = new FormGroup({
+             firstName: new FormControl('null, Validators.required'),
+             lastName: new FormControl('null, Validators.required'),
+             email: new FormControl(null, [
+             	    Validators.required,
+             	    Validators.pattern()
+             	]),
+             password: new FormControl('null, Validators.required'),
+		});
+	}
 }
