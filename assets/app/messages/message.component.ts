3f1bbd9d025307c777555dfcbe2d04490e543ed4
MichaelWiss <michael.wiss@gmail.com>
2017-02-27 18:40:13 -0600

    delete

--- assets/app/messages/message.component.ts
+++ assets/app/messages/message.component.ts
@@ -32,6 +32,9 @@ export class MessageComponent {
     	this.messageService.editMessage(this.message);
     }
     onDelete() {
-      this.messageService.deleteMessage(this.message);
+      this.messageService.deleteMessage(this.message)
+         .subscribe(
+           result => console.log(result)
+           );
     }
 }
--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -58,5 +58,8 @@ export class MessageService {
 
   deleteMessage(message: Message) {
   	this.messages.splice(this.messages.indexOf(message), 1);
+    return this.http.delete('http://locahost:3000/message' + message.messageId)
+         .map((response: Response) => response.json())
+         .catch((error: Response) => Observable.throw(error.json()));
   }
 }
